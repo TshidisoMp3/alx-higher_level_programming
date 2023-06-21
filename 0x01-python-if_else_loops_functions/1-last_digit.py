405503dbe199126449b203deb0eb390ee587c0fa
TshidisoMp3 <tshidisomorake18@gmail.com>
2023-06-21 11:41:31 +0200

    Python_Conditional_Statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-numsd = 0
-
+Tld = abs(number) % 10
 if number < 0:
-    number = number*-1
-    numsd = 1
-    Tld = number % 10
-if numsd == 1:
-    number = number*-1
-    Tld = Tld*-1
-    print(f"The last digit of {number:d} is {Tld:d} and is ", end="")
+    Tld = -Tld
+print(f"Last digit of {number:d} is {Tld:d} and is ", end="")
 if Tld > 5:
-    print(f"{Tld:d} is greater then 5")
+    print("greater than 5")
 elif Tld == 0:
-    print(f"{Tld:d} and is 0")
+    print("0")
 else:
-    print(f"{Tld:d} and is less then 6 and not 0")
+    print("less than 6 and not 0")
