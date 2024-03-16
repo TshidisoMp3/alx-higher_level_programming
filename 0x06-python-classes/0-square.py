da67e6716ee70856e76aae536d4f5cfc24bfb0c4
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-03-16 21:46:38 +0200

    Python_Classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    """An empty class that defines Square"""
+    """
+    An empty class that defines Square
+    This defines a square with an empty block of code
+    
+    """
     pass
