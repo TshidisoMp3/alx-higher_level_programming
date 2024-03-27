d9f919f6577f00d9306cc8662887155a061ad462
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-03-27 22:58:02 +0200

    Updates

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module defines the Class Square
+"""
+
+
+class Square:
+    """
+    This class defines a square:
+    - With a private instance attribute size
+    - With a public instance method area
+    - With a public instance method my_print
+    - With a public instance method position
+    - With a public instance method size
+    - With a public instance method size
+    """
+    
+    
