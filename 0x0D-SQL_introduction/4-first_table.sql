0cc19b0b0f0aeef4f50468cc4d449e2f50e352f8
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-03-29 18:32:24 +0200

    SQL_Introduction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- create the table first_table with the following columns:
-CREATE TABLE IF NOT EXITS first_table (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-)
+CREATE TABLE IF NOT EXITS `first_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+);
