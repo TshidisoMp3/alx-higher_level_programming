998aeee517464fa2c1a58a30abbac4556f5ad1da
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-03-30 22:14:09 +0200

    SQL_Introduction

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
--- list of all records in second_table
-SELECT * FROM second_table
+-- list of records in second_table
+-- which icludes score 
+SELECT `score`, `name`
+FROM second_table
+ORDER BY `score` DESC;
