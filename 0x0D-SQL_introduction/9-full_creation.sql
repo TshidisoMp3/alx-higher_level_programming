bebcf6d0cfd26bfa45efeb9352405aa192bf197e
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-03-30 21:36:47 +0200

    SQL_Introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,6 @@
 -- create table called second_table with the following info
-CREATE TABLE second_table (
-    id INT, name VARCHAR(256), score INT
-);
--- insert the following values into the table
-INSERT INTO second_table (id, name, score) VALUES
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 7),
-    (4, 'George', 8);
+CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
