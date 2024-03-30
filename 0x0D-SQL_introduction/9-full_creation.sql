db862084a95bf19c3472630eb0c1f5db7d1a980a
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-03-30 21:19:22 +0200

    SQL_Introduction

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- create table called second_table with the following info
+CREATE TABLE second_table (
+    id INT, name VARCHAR(256), score INT
+);
+-- insert the following values into the table
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 7),
+    (4, 'George', 8);
