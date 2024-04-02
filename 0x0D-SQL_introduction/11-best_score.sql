a2f95b286aedef46202fee78376d2d3288992f48
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-04-02 21:45:10 +0200

    SQL_Introduction

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
 -- list of all reacods on second_table with scotr >= 10
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `score` >= 10;
-ORDER BY `score` DESC;
+SELECT * FROM second_table WHERE score >= 10;
