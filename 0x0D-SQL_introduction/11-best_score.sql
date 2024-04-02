14ad5b987f4f8ab0bea50af3bf23b5e2ace50593
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-04-02 14:07:37 +0200

    SQL_Introduction

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- list of all reacods on second_table with scotr >= 10
+SELECT * FROM second_table WHERE 
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Insert a new row into the table
-Insert into first_table (id: 89, name: 'Best School');
+Insert into `first_table` (`id`, `name`) VALUE (89, Best School);
