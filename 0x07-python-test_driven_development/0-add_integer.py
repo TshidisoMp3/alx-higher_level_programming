5056bd19e5fe1c09ddebab9519cb1926ecb3be2e
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-04-21 20:22:47 +0200

    Python_TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""" Module that contains a function that adds two integers """
+"""
+This module contains a function that adds two integers
+"""
 
 def add_integer(a, b=98):
-    """Add two integers"""
-    if not isinstance(a, (int, float)):
+    """
+    Returns the sum of two integers of a and b
+    a and b must be integers or floats else typeError will be raised
+    a and b must be casted to integers before addition of floats
+    """
+    if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- CREATE a table called unique_id
+CREATE TABLE unique_id (
+    id   INT   DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
