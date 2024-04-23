7f4e78d263294b006a9576eca6f59b73b15c5824
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-04-23 23:53:27 +0200

    JS_Objects

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,5 +2,8 @@
 // Defines a class Rectangle that defines a rectangle with constructor of width and height
 
 module.exports = class Rectangle {
-    
-}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
