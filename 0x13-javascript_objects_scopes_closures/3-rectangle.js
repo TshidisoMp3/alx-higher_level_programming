c463913219282e739ec62205d58b7a14bf83fc4e
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-04-25 11:07:12 +0200

    JS_ Objects

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,16 +2,16 @@
 // class of rectangle with constructor of w and h were w and h are equal to 0 or not a positive integer and methods print the reactangle with character X
 
 module.exports = class Rectangle {
-   constructor (w, h) {
-     if (w > 0 && h > 0) {
-       this.width = w;
-       this.height = h;
-       }
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        [this.width, this.height] = [w, h];
+      }
     }
-    
+  
     print () {
       for (let i = 0; i < this.height; i++) {
         console.log('X'.repeat(this.width));
       }
     }
-}
+  };
+  
