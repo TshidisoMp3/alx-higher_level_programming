7e8aff9291b994d1bf215c1c267c74df6a8dce80
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-04-26 17:34:20 +0200

    JS_Objects

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,2 +1,24 @@
 #!/usr/bin/node
-// class of rectangle with constructor of w and h were w and h are equal to 0 or not a positive integer and methods print the reactangle with character X also create a rotate() instance that exchanges the width and the height of the rectangle and create 
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
