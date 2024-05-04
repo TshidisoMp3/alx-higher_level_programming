02a60a76ef7622ece988b2800a94a328a2b3bee6
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-05-04 20:46:03 +0200

    Python_ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Script that lists all states from the database hbtn_0e_0_usa"""
 
-import MySQLdb
 from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
