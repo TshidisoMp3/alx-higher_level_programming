6aa47a6264c70240d7c260dd3629147c461d324d
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-05-09 12:49:47 +0200

    Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Filter states by user input"""
+
+import sys
+import MySQLdb
+
+# Connect to a MySQL database
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+    )
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    c.close()
+    db.close()
