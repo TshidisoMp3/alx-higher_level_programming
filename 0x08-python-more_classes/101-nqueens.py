a02fcfa20945dfd7076ce3e89f51e9055c28c170
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-05-23 12:19:07 +0200

    python more  classes and objects

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""N Queens problem"""
+
+
+def isSafe(board, row, col):
+    """Check if a queen can be placed on board[row][col]"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
