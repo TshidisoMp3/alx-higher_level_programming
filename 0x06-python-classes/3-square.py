debab40402027dfb27660ae510661356b2c58af3
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-05-23 10:29:04 +0200

    python classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,15 +6,8 @@ This module defines the Class Square
 
 
 class Square:
-    """
-    This class defines a square:
-    - With a private instance attribute size
-    - With a public instance method area
-    - With a public instance method my_print
-    - With a public instance method position
-    - With a public instance method size
-    - With a public instance method size
-    """
+    """ Square class"""
+
 class Square:
     """ Square class"""
     def __init__(self, size=0):
@@ -24,7 +17,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
     def area(self):
         return self.__size * self.__size
-    
+ 
