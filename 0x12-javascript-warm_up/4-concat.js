477a8f4032a6f07550b876cc5dc1dde8389c95a7
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-06-18 19:36:00 +0200

    JS_warm-up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,18 +1,2 @@
 #!/usr/bin/node
-
-const args = process.argv;
-let i = 2;
-
-if (args.length === 2) {
-  console.log("No argument");
-}
-if (args.length === 3) {
-  console.log("Argument found");
-}
-if (args.length > 3) {
-  console.log("Arguments found");
-}
-
-for (i; i < args.length; i++) {
-  console.log(args[i]);
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
